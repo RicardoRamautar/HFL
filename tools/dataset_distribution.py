c1e4c908d8c3f80dfed58b689ce028cfa089e5f2
RicardoRamautar <ricardo.ramautar@gmail.com>
2026-01-19 13:49:35 +0100

    Combine different ways of distributing data across clients into a single file

--- tools/dataset_distribution.py
+++ tools/dataset_distribution.py
@@ -1,128 +1,89 @@
-# import json
-# import pickle
-# from pathlib import Path
-# from collections import defaultdict
-
-# output_file = Path("./data_distribution.json")
-# pth = '/home/rdr/Documents/master_thesis/data/nuscenes/nuscenes_infos_train.pkl'
-
-
-# # # Contains info of each scene
-# scene_file = Path('./scene.json')
-# log_file = Path('./log.json')
-
-# if not scene_file.exists() or not log_file.exists():
-#     raise FileNotFoundError("Could not find nuScenes metadata files")
-
-# # Load metadata
-# with open(scene_file, "r") as f:
-#     scenes = json.load(f)
-
-# with open(log_file, "r") as f:
-#     logs = json.load(f)
-
-# with open(pth, "rb") as f:
-#     data = pickle.load(f)
-
-# data_tokens = [data['infos'][i]['scene_token'] for i in range(len(data['infos']))]
-
-# # Dict(log token: location)
-# log_token_to_location = {
-#     log["token"]: log["location"]
-#     for log in logs
-# }
-
-# # Group scenes by location
-# scenes_by_location = defaultdict(list)
-# for scene in scenes:
-#     scene_name = scene["name"]          # Scene name
-#     log_token = scene["log_token"]      # Log token
-#     location = log_token_to_location[log_token]
-
-#     scene_token = scene['token']
-#     if scene_token in data_tokens:
-#         scenes_by_location[location].append(scene_name)
-
-# # Build manifest: edge -> clients -> scenes
-# # Every location represents an edge, every scene represents a client
-# manifest = {"edges": {}}
-# client_id = 0
-
-# for location, scene_names in sorted(scenes_by_location.items()):
-#     manifest["edges"][location] = {"clients": {}}
-
-#     for scene_name in sorted(scene_names):
-#         cid = f"client_{client_id:04d}"
-#         manifest["edges"][location]["clients"][cid] = {
-#             "scenes": [scene_name]
-#         }
-#         client_id += 1
-
-# # Write output
-# with open(output_file, "w") as f:
-#     json.dump(manifest, f, indent=2)
-
-# # Print summary (important sanity check)
-# print("Manifest written to:", output_file)
-# print("Number of edges:", len(manifest["edges"]))
-# print("Clients per edge:")
-# for edge, data in manifest["edges"].items():
-#     print(f"  {edge}: {len(data['clients'])} clients")
-
-
 import json
 import pickle
 from pathlib import Path
 from collections import defaultdict
 import random
 
-# Paths to nuScenes metadata files
-pkl_file = "/tudelft.net/staff-umbrella/rdramautar/datasets/nuscenes_infos/nuscenes_infos_train.pkl"
-scene_file = Path('/tudelft.net/staff-umbrella/IntelligentVehiclesPublicDatasets/nuscenes/v1.0-trainval/scene.json')
-log_file = Path('/tudelft.net/staff-umbrella/IntelligentVehiclesPublicDatasets/nuscenes/v1.0-trainval/log.json')
+def print_manifest(manifest):
+    # Print summary
+    total_scenes = 0
+    print("Number of edges:", len(manifest["edges"]))
+    print("Clients per edge:")
+    for edge, data in manifest["edges"].items():
+        print(f"  {edge}: {len(data['clients'])} clients")
 
-output_file = Path("/tudelft.net/staff-umbrella/rdramautar/HFL/data/data_distribution_2.json")
+        for client, scenes in data['clients'].items():
+            num_scenes = len(scenes['scenes'])
+            total_scenes += num_scenes
+            print(f"    Client {client} has {num_scenes} scenes")
 
-num_clients_per_location = 2
-random.seed(42)
-
-
-# Load metadata
-with open(scene_file, "r") as f:
-    scenes = json.load(f)
-with open(log_file, "r") as f:
-    logs = json.load(f)
-with open(pkl_file, "rb") as f:
-    data = pickle.load(f)
-
-# Extract all scene tokens in dataset
-data_tokens = [info['scene_token'] for info in data['infos']]
-
-# Dict(log token: location)
-log_token_to_location = {
-    log["token"]: log["location"]
-    for log in logs
-}
+    print(f"Total number of scenes: {total_scenes}")
 
 
-# Group scenes by location
-scenes_by_location = defaultdict(list)
-for scene in scenes:
-    scene_name = scene["name"]
-    log_token = scene["log_token"]
-    location = log_token_to_location[log_token]
+def store_manifest(manifest, file):
+    with open(file, "w") as f:
+        json.dump(manifest, f, indent=2)
 
-    scene_token = scene['token']
-    if scene_token in data_tokens:
-        scenes_by_location[location].append(scene_name)
+def client_per_scene(scenes, logs, data_tokens):
+    # Dict(log token: location)
+    log_token_to_location = {
+        log["token"]: log["location"]
+        for log in logs
+    }
 
-def assign_data(scenes_by_location, cpl):
+    # Group scenes by location
+    scenes_by_location = defaultdict(list)
+    for scene in scenes:
+        scene_name = scene["name"]          # Scene name
+        log_token = scene["log_token"]      # Log token
+        location = log_token_to_location[log_token]
+
+        scene_token = scene['token']
+        if scene_token in data_tokens:
+            scenes_by_location[location].append(scene_name)
+
+    # Build manifest: edge -> clients -> scenes
+    # Every location represents an edge, every scene represents a client
+    manifest = {"edges": {}}
+    client_id = 0
+
+    for location, scene_names in sorted(scenes_by_location.items()):
+        manifest["edges"][location] = {"clients": {}}
+
+        for scene_name in sorted(scene_names):
+            cid = f"client_{client_id:04d}"
+            manifest["edges"][location]["clients"][cid] = {
+                "scenes": [scene_name]
+            }
+            client_id += 1
+
+    return manifest
+
+
+def scenes_per_client(scenes, logs, data_tokens, cpl):
     def split_list(lst, num):
         k, m = divmod(len(lst), num)
         return [
             lst[i*k + min(i, m):(i+1)*k + min(i+1, m)]
             for i in range(num)
         ]
+
+    # Dict(log token: location)
+    log_token_to_location = {
+        log["token"]: log["location"]
+        for log in logs
+    }
+
+    # Group scenes by location
+    scenes_by_location = defaultdict(list)
+    for scene in scenes:
+        scene_name = scene["name"]
+        log_token = scene["log_token"]
+        location = log_token_to_location[log_token]
+
+        scene_token = scene['token']
+        if scene_token in data_tokens:
+            scenes_by_location[location].append(scene_name)
 
     manifest = {"edges": {}}
     client_id = 0
@@ -142,31 +103,55 @@ def assign_data(scenes_by_location, cpl):
 
     return manifest
 
-manifest = assign_data(scenes_by_location, num_clients_per_location)
 
-    # Write output
-with open(output_file, "w") as f:
-    json.dump(manifest, f, indent=2)
+def client_per_log(scenes, logs, scene_tokens):
+    manifest = {"edges": {}}
+    client_id = 0
+    for log in logs:
+        location = log['location']
+        log_token = log['token']
 
-all_scenes = []
-for edge in manifest["edges"].values():
-    for client in edge["clients"].values():
-        all_scenes.extend(client["scenes"])
+        if location not in manifest["edges"]:
+            manifest["edges"][location] = {"clients": {}}
 
-assert len(all_scenes) == len(set(all_scenes)), "Duplicate scenes detected"
+        client_scenes = [scene['name'] for scene in scenes if scene['log_token'] == log_token and scene['token'] in scene_tokens]
 
-# Print summary
-total_scenes = 0
-print("Manifest written to:", output_file)
-print("Number of edges:", len(manifest["edges"]))
-print("Clients per edge:")
-for edge, data in manifest["edges"].items():
-    print(f"  {edge}: {len(data['clients'])} clients")
+        if client_scenes:
+            cid = f"client_{client_id:04d}"
+            manifest["edges"][location]["clients"][cid] = {
+                "scenes": client_scenes
+            }
+            client_id += 1
 
-    for client, scenes in data['clients'].items():
-        num_scenes = len(scenes['scenes'])
-        total_scenes += num_scenes
-        print(f"    Client {client} has {num_scenes}")
+    return manifest
 
-print(f"Total number of scenes: {total_scenes}")
 
+def main():
+    # Paths to nuScenes metadata files
+    pkl_file = "/home/rdr/Documents/master_thesis/data/nuscenes/nuscenes_infos_train.pkl"
+    scene_file = Path('./scene.json')
+    log_file = Path('./log.json')
+
+    output_file = Path("./data_distribution_4.json")
+
+    # Load metadata
+    with open(scene_file, "r") as f:
+        scenes = json.load(f)
+    with open(log_file, "r") as f:
+        logs = json.load(f)
+    with open(pkl_file, "rb") as f:
+        data = pickle.load(f)
+
+    # Extract all scene tokens in dataset
+    data_tokens = [info['scene_token'] for info in data['infos']]
+
+    # manifest = client_per_scene(scenes, logs, data_tokens)
+    # manifest = scenes_per_client(scenes, logs, data_tokens, 2)
+    manifest = client_per_log(scenes, logs, data_tokens)
+
+    store_manifest(manifest, output_file)
+
+    print_manifest(manifest)
+
+if __name__=='__main__':
+    main()
