71dcf09b8ee3383e89810a3288a94240be7c35ee
RicardoRamautar <ricardo.ramautar@gmail.com>
2026-02-17 09:03:19 +0100

    Implement test script to evaluate state dicts stored during HFL process

---
+++ scripts/test.py
@@ -0,0 +1,36 @@
+from hfl.coordinator import Coordinator
+from mmcv import print_log
+
+def main():
+    print_log(f"Constructing coordinator...", logger='root' )
+
+    base_lr = 0.0001 / 4
+    num_local_rounds = 1
+    num_edge_rounds = 2
+    num_global_rounds = 7
+
+    cloud = Coordinator(
+        work_root = "/tudelft.net/staff-umbrella/rdramautar/HFL/experiments/exp_020",
+        base_cfg_path = "/tudelft.net/staff-umbrella/rdramautar/HFL/configs/cmt_lidar_cyclic_lr.py",
+        val_cfg_path = "/tudelft.net/staff-umbrella/rdramautar/HFL/configs/cmt_lidar_cyclic_lr_val.py",
+        manifest_path = "/tudelft.net/staff-umbrella/rdramautar/HFL/data/iid_day_night_2edges_4clients.json",
+        init_ckpt_path = None,
+        num_local_rounds = num_local_rounds,
+        num_edge_rounds = num_edge_rounds,
+        num_global_rounds = num_global_rounds,
+        lr_cfg = {
+            'policy': "cyclic",
+            'total_epochs': num_local_rounds * num_edge_rounds * num_global_rounds,
+            'initial_lr': base_lr,
+            'min_lr': 0.005*base_lr,
+            'max_lr': 6*base_lr
+        },
+        token_to_name_path = "/tudelft.net/staff-umbrella/rdramautar/HFL/data/scene_name_to_token.json",
+        seed = 0
+    )
+    print_log(f"Coordinator constructed. Starting training...", logger='root' )
+
+    cloud.validate('/tudelft.net/staff-umbrella/rdramautar/HFL/experiments/exp_016/global_round_4/edge_1/edge_round_1/client_2/weights.pth')
+
+if __name__ == '__main__':
+    main()
